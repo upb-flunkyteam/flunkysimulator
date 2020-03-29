a65c71c51629aab0424553728b37037a0b28e271
Jan Drees <jdrees@mailbox.org>
2020-03-29 15:16:41 +0200

    Fixing renamed public folder in javascript

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
     res.render('homepage');
 });
 
-app.use(express.static(path.join(__dirname, 'public_html')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
