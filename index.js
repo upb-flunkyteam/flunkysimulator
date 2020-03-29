93e04114d38654b33b4c15b03ab9fc5041ad2223
Jan Drees <jdrees@mailbox.org>
2020-03-29 15:08:54 +0200

    Migrating to pug, displaying simple hello world page

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+let app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public_html')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set('view engine', 'pug')
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+app.use(express.static(path.join(__dirname, 'public_html')))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
