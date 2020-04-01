81d02c179fcc2087417e74afa5d08b1706f25c40
Jan Drees <jdrees@mailbox.org>
2020-04-01 22:03:37 +0200

    Fixing path issues again

--- client/webserver.js
+++ client/webserver.js
@@ -6,12 +6,12 @@ let app = express();
 
 app.set('view engine', 'pug');
 
-app.set('views', path.join(__dirname, 'client/views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('homepage');
 });
 
-app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
