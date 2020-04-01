c28fe01ac0ffeb65d4d8bede0d64b138bfdfde0b
Jan Drees <jdrees@mailbox.org>
2020-04-01 23:13:16 +0200

    fixing webpack output paths

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ module.exports = {
     './client/public/js/client.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/client/public/js',
+    path: __dirname + '/public/js',
+    publicPath: '/js',
     filename: 'main.js'
   }
 } 
