dcb590fef14cd3a786a8808ccbb24bbddc89ffb9
jan <janhentie@web.de>
2020-04-02 20:21:56 +0200

    add kotlin server project

---
+++ serverKotlin/src/main/kotlin/main.kt
@@ -0,0 +1,64 @@
+
+
+package simulator
+
+import de.flunkyteam.endpoints.projects.simulator.*
+import io.grpc.ServerBuilder
+import io.grpc.stub.StreamObserver
+
+
+class FlunkyServer(): SimulatorGrpc.SimulatorImplBase() {
+
+    override fun throw_(request: ThrowReq?, responseObserver: StreamObserver<ThrowResp>?) {
+        super.throw_(request, responseObserver)
+    }
+
+    override fun registerPlayer(request: RegisterPlayerReq?, responseObserver: StreamObserver<RegisterPlayerResp>?) {
+        super.registerPlayer(request, responseObserver)
+    }
+
+    override fun kickPlayer(request: KickPlayerReq?, responseObserver: StreamObserver<KickPlayerResp>?) {
+        super.kickPlayer(request, responseObserver)
+    }
+
+    override fun resetGame(request: ResetGameReq?, responseObserver: StreamObserver<ResetGameResp>?) {
+        super.resetGame(request, responseObserver)
+    }
+
+    override fun selectThrowingPlayer(
+        request: SelectThrowingPlayerReq?,
+        responseObserver: StreamObserver<SelectThrowingPlayerResp>?
+    ) {
+        super.selectThrowingPlayer(request, responseObserver)
+    }
+
+    override fun sendMessage(request: SendMessageReq?, responseObserver: StreamObserver<SendMessageResp>?) {
+        super.sendMessage(request, responseObserver)
+    }
+
+    override fun streamState(request: StreamStateReq?, responseObserver: StreamObserver<StreamStateResp>?) {
+        super.streamState(request, responseObserver)
+    }
+
+    override fun streamEvents(request: StreamEventsReq?, responseObserver: StreamObserver<StreamEventsResp>?) {
+        super.streamEvents(request, responseObserver)
+    }
+
+    override fun streamLog(request: LogReq?, responseObserver: StreamObserver<LogResp>?) {
+        super.streamLog(request, responseObserver)
+    }
+}
+
+
+fun main(args: Array<String>) {
+    var server = ServerBuilder.forPort(80).addService(FlunkyServer()).build();
+
+    server.start()
+
+    println("Starting Server")
+
+    println("Press any key to cancel")
+    readLine()
+
+}
+
