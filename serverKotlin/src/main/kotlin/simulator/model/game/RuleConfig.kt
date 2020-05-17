c56ead83347a304af42f1f0336570d45ee26cf3b
jan <janhentie@web.de>
2020-05-17 14:59:14 +0200

    Server: Fix set resting period constant to 4 seconds instead of 4 milliseconds

--- serverKotlin/src/main/kotlin/simulator/model/game/RuleConfig.kt
+++ serverKotlin/src/main/kotlin/simulator/model/game/RuleConfig.kt
@@ -2,7 +2,7 @@ package simulator.model.game
 
 import de.flunkyteam.endpoints.projects.simulator.RuleConfig
 
-data class RuleConfig(val restingPeriodMilliseconds: Int = 4){
+data class RuleConfig(val restingPeriodMilliseconds: Int = 4000){
 
     fun toGrpc(): RuleConfig = RuleConfig.newBuilder()
         .setRestingPeriodLength(restingPeriodMilliseconds.toLong())
